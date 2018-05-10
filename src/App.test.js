a3a95e307c16a59cca752845d694d7e354c4d5c1
Piotr Zientara <314zientara@gmail.com>
2018-05-10 22:47:03 +0200

    Add snapshot test and section test with styled components config

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import 'jest-styled-components';
+
 configure({adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  const wrapper  = shallow(<App />);
-  console.log(wrapper.debug());
+describe('<App />', () => {
+    const wrapper = shallow(<App />);
+    it('should contain sections', () => {
+        expect(wrapper.find('Header').length).toBe(1);
+        expect(wrapper.find('WorkshopForm').length).toBe(1);
+        expect(wrapper.find('TopicsList').length).toBe(1);
+    });
+
+    it('should match snapshot', () => {
+        expect(wrapper).toMatchSnapshot();
+    })
 });
