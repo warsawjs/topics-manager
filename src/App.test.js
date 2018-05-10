cc190de1cb69e9341d273772f11f9626cc3457ad
Piotr Zientara <314zientara@gmail.com>
2018-05-10 22:23:02 +0200

    Add test configuration

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({adapter: new Adapter() });
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper  = shallow(<App />);
+  console.log(wrapper.debug());
 });
